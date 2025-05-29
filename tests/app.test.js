6e49c251f5d77ef01e595ba46c3b1635719fe041
Saniwaa <saniwaa16@gmail.com>
2025-05-29 18:42:51 +0000

    Initial commit with application code

---
+++ tests/app.test.js
@@ -0,0 +1,11 @@
+// tests/app.test.js
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+    it('should return Hello, World!', async () => {
+        const res = await request(app).get('/');
+        expect(res.text).toBe('Hello, World! This is my Node.js application.');
+        expect(res.statusCode).toBe(200);
+    });
+});
